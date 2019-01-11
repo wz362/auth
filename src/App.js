3298f846382fdd3a29959c2103c90d4c1643ce4d
Wenjia Zhang <wenjiazhang@OLBBs-MacBook-Pro-3.local>
2019-01-11 12:36:20 -0500

    Logout  with firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { View } from 'react-native';
+import { CardSection, Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: "AIzaSyBPNZZDnxKrddodZm9eeoideVOQxndbvEc",
@@ -14,13 +16,43 @@ class App extends Component {
             storageBucket: "auth-bb592.appspot.com",
             messagingSenderId: "536064012760"
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </CardSection>
+                    );
+            case false:
+                return <LoginForm />;
+            default:
+                return (
+                    <CardSection>
+                        <Spinner />
+                    </CardSection>
+                );
+        } 
     }
 
     render() {
         return (
             <View>
-                <Header headerText="Authentication"/>
-                <LoginForm />
+                <Header headerText="Authentication" />
+                {this.renderContent()}
+                
             </View>
         )
     };
